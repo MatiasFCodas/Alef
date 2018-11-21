c389666bbc4b81a951bc182e42d5bee556d7f9ad
Saitam000 <matiascodas@gmail.com>
2018-11-21 11:03:00 -0200

    Começa a fase2

---
+++ Alef/Alef/Inter_2018_Prototipo/Prototipo/Prototipo/Assets/ComecaFase02.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ComecaFase02 : MonoBehaviour {
+
+    public Animator anim;
+    public Animator portaAnim;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+            portaAnim.SetTrigger("AbriPorta");
+    }
+
+    public void FadeToLevel()
+    {
+        anim.SetTrigger("FadeOut");
+    }
+
+    private void FadeComplete()
+    {
+        SceneManager.LoadScene("Level_02");
+    }
+}
