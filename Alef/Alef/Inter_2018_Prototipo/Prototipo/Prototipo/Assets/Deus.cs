d00a06a4facce60438153848d2c92aa4957c9333
Saitam000 <matiascodas@gmail.com>
2018-11-21 14:17:32 -0200

    Fala Deus

---
+++ Alef/Alef/Inter_2018_Prototipo/Prototipo/Prototipo/Assets/Deus.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Deus : MonoBehaviour {
+
+    public AudioSource falaDeus;
+    public CinemachineFreeLook camDeus;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+     void OnTriggerEnter(Collider other)
+    {
+        falaDeus.Play();
+        camDeus.Priority = 12;
+    }
+}
