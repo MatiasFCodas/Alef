3f29e407880bcd5762d648411000bd48281a71fd
Cesar-Casadoleo <cesarnevesmosca@yahoo.com>
2018-11-23 13:21:30 -0200

    Tentando arrumar uns bugs

---
+++ Alef/Alef/Inter_2018_Prototipo/Prototipo/Prototipo/Assets/_AKhalil/Scripts/Movimento_Petalas_Lerp.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento_Petalas_Lerp : MonoBehaviour {
+
+
+    /// <Explicação>
+    /// 
+    /// 
+    /// 
+    /// </Explicação>
+
+
+    // Pontos de referência para a movimentação das petalas
+    Transform startingPoint;
+    Transform finishingPoint;
+
+
+    // Velocidade de movimento da petala até a referência (Lerp)
+    public float speed;
+
+
+    // Referencias das petalas que irão girar
+    // Caso haja a necessidade de refazer as associações apagar "[HideInInspector]"
+    [HideInInspector] public Transform Alterado_01;
+    [HideInInspector] public Transform Alterado_02;
+    [HideInInspector] public Transform Alterado_03;
+    [HideInInspector] public Transform Referencia_01;
+    [HideInInspector] public Transform Referencia_02;
+    [HideInInspector] public Transform Referencia_03;
+
+
+
+    public Totem_Manager totem;
+
+
+    // Quanto no puzzle o jogador avançou / Quantas partes da flor ele liberou
+    public int estagio; 
+
+
+    // Variável que auxilia o calculo de movimentos, dizendo quantos graus cada pétala se move por vez
+    public float distancia;
+
+
+    // Diz qual a posição das possibilidades o totem está, auxiliando o algoritmo a saber qual movimento deve ser
+    // feito a seguir
+    public float posicao; 
+
+
+    // Variável que auxilia o cálculo de movimentos a limitar o número de movimentos da pétala
+    public int ajuste;
+
+    // Diz qual das pétalas está selecionada no momento
+    [HideInInspector]public float petalaSelecionada = 1;
+
+
+    // Armazena a posição atual da petala selecionada, impedindo que o número de movimentos realizado por uma peça
+    // influencíe nas outras
+    float posicaoArmazenada_01;
+    float posicaoArmazenada_02;
+    float posicaoArmazenada_03;
+
+
+    void Start()
+    {
+        posicao = 0;
+    }
+
+
+
+    void FixedUpdate()
+    {
+        MudaPetala();
+        totem.totemSelecionado = petalaSelecionada;
+        petalaSelecionada = totem.totemSelecionado;
+    }
+
+
+
+
+
+
+    void MudaPetala()
+    {
+        if (petalaSelecionada == 1)
+        {
+            distancia = 30;
+            ajuste = 0;
+            posicao = posicaoArmazenada_01;
+            startingPoint = Alterado_01;
+            finishingPoint = Referencia_01;
+            GiraPetala();
+            posicaoArmazenada_01 = posicao;
+        }
+        else if (petalaSelecionada == 2)
+        {
+            distancia = 45;
+            ajuste = 1;
+            posicao = posicaoArmazenada_02;
+            startingPoint = Alterado_02;
+            finishingPoint = Referencia_02;
+            GiraPetala();
+            posicaoArmazenada_02 = posicao;
+        }
+        else if (petalaSelecionada == 3)
+        {
+            distancia = 60;
+            ajuste = 2;
+            posicao = posicaoArmazenada_03;
+            startingPoint = Alterado_03;
+            finishingPoint = Referencia_03;
+            GiraPetala();
+            posicaoArmazenada_03 = posicao;
+        }
+
+        if (estagio == 2) // Alteração entre as petalas caso o jogador esteja no nivel 2
+        {
+            if (Input.GetKeyDown(KeyCode.W)) //===== CIMA =====//
+            {
+                if (petalaSelecionada >= 2) petalaSelecionada--;
+                else petalaSelecionada = 2;
+            }
+
+            if (Input.GetKeyDown(KeyCode.S)) //===== BAIXO =====//
+            {
+                if (petalaSelecionada <= 1) petalaSelecionada++;
+                else petalaSelecionada = 1;
+            }
+        } // Alteração entre as petalas caso o jogador esteja no nivel 2
+
+        if (estagio == 3)// Alteração entre as petalas caso o jogador esteja no nivel 3
+        {
+            if (Input.GetKeyDown(KeyCode.W)) //===== CIMA =====//
+            {
+                if (petalaSelecionada >= 2) petalaSelecionada--;
+                else petalaSelecionada = 3;
+            }
+
+            if (Input.GetKeyDown(KeyCode.S)) //===== BAIXO =====//
+            {
+                if (petalaSelecionada <= 2) petalaSelecionada++;
+                else petalaSelecionada = 1;
+            }
+        }// Alteração entre as petalas caso o jogador esteja no nivel 3
+
+    }
+
+
+
+    void GiraPetala()
+    {
+
+        Quaternion newFinishingPoint = finishingPoint.rotation;
+        Quaternion smootheredThing = Quaternion.Lerp(startingPoint.rotation, newFinishingPoint, speed);
+        startingPoint.rotation = smootheredThing;
+
+
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            totem.speed = 1;
+            if (posicao > -distancia * (estagio - ajuste))
+            {
+                finishingPoint.Rotate(Vector3.up * -distancia); // Subtrair variavel de estados
+                posicao -= distancia;
+            }
+            else
+            {
+                finishingPoint.Rotate(Vector3.up * distancia * ((estagio - ajuste) * 2)); // Subtrair variavel de estados
+                posicao = distancia * (estagio - ajuste);
+            }
+        }
+
+
+
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            totem.speed = 1;
+            if (posicao < distancia * (estagio - ajuste))
+            {
+                finishingPoint.Rotate(Vector3.up * distancia); // Subtrair variavel de estados
+                posicao += distancia;
+            }
+            else
+            {
+                finishingPoint.Rotate(Vector3.up * -distancia * ((estagio - ajuste) * 2)); // Subtrair variavel de estados
+                posicao = -distancia * (estagio - ajuste);
+            }
+        }
+    }
+}
---
+++ Alef/Alef/Inter_2018_Prototipo/Prototipo/Prototipo/Assets/_AKhalil/Scripts/Totem_Manager.cs
@@ -0,0 +1,306 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Totem_Manager : MonoBehaviour
+{
+
+    /// <Explicação>
+    /// 
+    /// 
+    /// 
+    /// </Explicação>
+    
+    
+    [HideInInspector]public Transform startingPoint;
+    [HideInInspector]public Transform finishingPoint;
+
+    // Velocidade em que o totem se move no Lerp
+    [HideInInspector]public float speed = 0.125f;
+
+
+    // Referencias dos totens que irão girar
+    // Caso haja a necessidade de refazer as associações apagar "[HideInInspector]"
+    public Transform Alterado_01;
+    public Transform Alterado_02;
+    public Transform Alterado_03;
+    public Transform Referencia_01;
+    public Transform Referencia_02;
+    public Transform Referencia_03;
+
+
+
+    // Possíveis posições do primeiro (1º) totem
+    public float t1Pos1, t1Pos2, t1Pos3;
+    public float t1PosN1, t1PosN2, t1PosN3;
+
+
+    // Possíveis posições do segundo (2º) totem
+    public float t2Pos1, t2Pos2;
+    public float t2PosN1, t2PosN2;
+
+
+    // Possíveis posições do terceiro (3º) totem
+    public float t3Pos1;
+    public float t3PosN1;
+
+
+    // Diz qual dos totens está selecionado no momento, alterando assim o quanto o totem se movimenta
+    // e assim também qual deles se movimentará
+    public float totemSelecionado;
+
+
+    // Variaveis que dizem quanto o totem vai se mover, sempre são alteradas caso mude de totem 
+    // por conta da função "Ajusta Posições" recebendo os valores que forem preenchidos nas variáveis
+    float Pos1, Pos2, Pos3; 
+    float PosNeg1, PosNeg2, PosNeg3;
+
+    
+    // Diz qual a posição das possibilidades o totem está, auxiliando o algoritmo a saber qual movimento deve ser
+    // feito a seguir
+    public int modo;
+
+
+    // Variaveis para armazenar em qual ponto a variavel "modo" se encontrava antes de trocar para outro 
+    // totem, evitando assim que os totens se movam além do que é permitido
+    int modoArmazenado01;
+    int modoArmazenado02;
+    int modoArmazenado03;
+         
+
+    // Referencia para a variável "estagio" a qual diz quantos totens/petalas o jogador já liberou
+    // sendo assim o quanto ele avançou no level
+    public Movimento_Petalas_Lerp petal;
+
+    
+
+    void FixedUpdate()
+    {
+        GiraTotem();
+        petal.petalaSelecionada = totemSelecionado;
+        totemSelecionado = petal.petalaSelecionada;                
+    }
+
+
+
+    void GiraTotem()
+    {
+        Quaternion newFinishingPoint = finishingPoint.rotation;
+        Quaternion smootheredThing = Quaternion.Lerp(startingPoint.rotation, newFinishingPoint, speed);
+        startingPoint.rotation = smootheredThing;
+
+        if (totemSelecionado == 1)
+        {
+            modo = modoArmazenado01;
+            AjustaPosicoes(t1Pos1, t1Pos2, t1Pos3, t1PosN1, t1PosN2, t1PosN3);
+            MoveTotem(1);
+            modoArmazenado01 = modo;
+            startingPoint = Alterado_01;
+            finishingPoint = Referencia_01;
+        }
+
+        if (totemSelecionado == 2)
+        {
+            modo = modoArmazenado02;
+            AjustaPosicoes(t2Pos1, t2Pos2, 0, t2PosN1, t2PosN2, 0);
+            MoveTotem(2);
+            modoArmazenado02 = modo;
+            startingPoint = Alterado_02;
+            finishingPoint = Referencia_02;
+        }
+
+        if (totemSelecionado == 3)
+        {
+            modo = modoArmazenado03;
+            AjustaPosicoes(t3Pos1, 0, 0, t3PosN1, 0, 0);
+            MoveTotem(3);
+            modoArmazenado03 = modo;
+            startingPoint = Alterado_03;
+            finishingPoint = Referencia_03;
+        }
+
+
+        if (petal.estagio == 2) // Alteração entre os totens caso o jogador esteja no nivel 2
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow)) //===== CIMA =====//
+            {
+                if (totemSelecionado >= 2) totemSelecionado--;
+                else totemSelecionado = 2;
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow)) //===== BAIXO =====//
+            {
+                if (totemSelecionado <= 1) totemSelecionado++;
+                else totemSelecionado = 1;
+            }
+        }// Alteração entre os totens caso o jogador esteja no nivel 2
+
+        if (petal.estagio == 3)// Alteração entre os totens caso o jogador esteja no nivel 3
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow)) //===== CIMA =====//
+            {
+                if (totemSelecionado >= 2) totemSelecionado--;
+                else totemSelecionado = 3;
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow)) //===== BAIXO =====//
+            {
+                if (totemSelecionado <= 2) totemSelecionado++;
+                else totemSelecionado = 1;
+            }
+        }// Alteração entre os totens caso o jogador esteja no nivel 3
+    }
+
+
+
+    void AjustaPosicoes(float val1,float val2,float val3, float valNeg1,float valNeg2, float valNeg3)
+    {
+        Pos1 = val1;
+        Pos2 = val2;
+        Pos3 = val3;
+
+        PosNeg1 = valNeg1;
+        PosNeg2 = valNeg2;
+        PosNeg3 = valNeg3;
+    }
+
+
+
+    void MoveTotem(int qualTotem) //Dependendo de quanto o jogador avançou na fase ele tem um certo limite
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            speed = 0.05f;
+
+            if (modo == -3)
+            {
+                finishingPoint.Rotate(Vector3.up * -PosNeg3);
+                modo++;
+                return;
+            }
+
+            if (modo == -2)
+            {
+                finishingPoint.Rotate(Vector3.up * -PosNeg2);
+                modo++;
+                return;
+            }
+
+            if (modo == -1)
+            {
+                finishingPoint.Rotate(Vector3.up * -PosNeg1);
+                modo++;
+                return;
+            }
+
+            if (modo == 0)
+            {
+                finishingPoint.Rotate(Vector3.up * -Pos1);
+                modo++;
+                return;
+            }
+
+            if (modo == 1)
+            {
+                if (qualTotem >=3)
+                {
+                    finishingPoint.Rotate(Vector3.up * ((PosNeg1 + Pos1) * 1));
+                    modo = -1;
+                    return;
+                }
+                finishingPoint.Rotate(Vector3.up * -Pos2);
+                modo++;
+                return;
+            }
+
+            if (modo == 2)
+            {
+                if (qualTotem >= 2)
+                {
+                    finishingPoint.Rotate(Vector3.up * ((PosNeg2 + PosNeg1 + Pos1 + Pos2) * 1));
+                    modo = -2;
+                    return;
+                }
+                finishingPoint.Rotate(Vector3.up * -Pos3);
+                modo++;
+                return;
+            }
+
+            if (modo == 3)
+            {
+                finishingPoint.Rotate(Vector3.up * ((PosNeg3 + PosNeg2 + PosNeg1 + Pos1 + Pos2 + Pos3) * 1));
+                modo = -3;
+                return;
+            }
+        }
+
+
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            speed = 0.05f;
+            
+            if(modo == -3)
+            {
+                finishingPoint.Rotate(Vector3.up * ((PosNeg3 + PosNeg2 + PosNeg1 + Pos1 + Pos2 + Pos3) * -1));
+                modo = 3;
+                return;
+            }
+
+            if (modo == -2)
+            {
+                if (qualTotem >= 2)
+                { 
+                    finishingPoint.Rotate(Vector3.up * ((PosNeg2 + PosNeg1 + Pos1 + Pos2) * -1));
+                    modo = 2;
+                    return;
+                }
+                finishingPoint.Rotate(Vector3.up * PosNeg3);
+                modo--;
+                return;
+
+            }
+            
+            if (modo == -1)
+            {
+                if (qualTotem >= 3)
+                {
+                    finishingPoint.Rotate(Vector3.up * ((PosNeg1 + Pos1) * -1));
+                    modo = 1;
+                    return;
+                }
+                finishingPoint.Rotate(Vector3.up * PosNeg2);
+                modo--;
+                return;                
+            }
+
+            if (modo == 0)
+            {
+                finishingPoint.Rotate(Vector3.up * PosNeg1);
+                modo--;
+                return;                
+            }
+
+            if (modo == 1)
+            {
+                finishingPoint.Rotate(Vector3.up * Pos1);
+                modo--;
+                return;
+            }
+
+            if (modo == 2)
+            {
+                finishingPoint.Rotate(Vector3.up * Pos2);
+                modo--;
+                return;
+            }
+
+            if (modo == 3)
+            {
+                finishingPoint.Rotate(Vector3.up * Pos3);
+                modo--;
+                return;
+            }
+        }
+    }
+}
