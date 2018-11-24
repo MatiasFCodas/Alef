b06ab172a92773fd18b235dc72fd626420ff51a7
Cesar-Casadoleo <cesarnevesmosca@yahoo.com>
2018-11-24 01:39:56 -0200

    Ilha com água

---
+++ Alef/Alef/Inter_2018_Prototipo/Prototipo/Prototipo/Assets/moveagua.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveagua : MonoBehaviour {
+
+	private Renderer agua;
+	// Use this for initialization
+	void Start () {
+		agua = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agua.material.mainTextureOffset += new Vector2(.01f, .03f);
+
+
+	}
+}
