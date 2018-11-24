4e3edea4c08ae8fe6b59c7dad18ac2dac0fe94e5
Cesar-Casadoleo <cesarnevesmosca@yahoo.com>
2018-11-24 16:34:29 -0200

    Camera movendo no menu

--- Alef/Alef/Inter_2018_Prototipo/Prototipo/Prototipo/Assets/movecam.cs
+++ Alef/Alef/Inter_2018_Prototipo/Prototipo/Prototipo/Assets/movecam.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class movecam : MonoBehaviour {
 
+	private Transform cam;
 	// Use this for initialization
 	void Start () {
-		
+		cam = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		cam.Rotate(Vector3.up, .5f);
 	}
 }
