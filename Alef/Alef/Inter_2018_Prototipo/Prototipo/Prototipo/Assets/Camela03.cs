60294e624f82bda34fb8ae36e3c35616a7cbeab1
Saitam000 <matiascodas@gmail.com>
2018-11-25 18:57:52 -0200

    Camelo Legenda

---
+++ Alef/Alef/Inter_2018_Prototipo/Prototipo/Prototipo/Assets/Camela03.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camela03 : MonoBehaviour {
+
+    public AudioSource camela03;
+
+    //public Animator animCamelo;
+
+    public GameObject legenda03;
+
+    public CharController parado;
+
+    public bool isComplete;
+
+    public GameObject camelo;
+
+
+
+
+    void Update()
+    {
+        if (isComplete == true)
+        {
+            legenda03.SetActive(false);
+            //animCamelo.SetTrigger("AndaCamela");
+
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine("NarraCamela02");
+
+    }
+
+    IEnumerator NarraCamela02()
+    {
+        camela03.Play();
+        legenda03.SetActive(true);
+
+        yield return new WaitForSeconds(15);
+
+        isComplete = true;
+
+        StopAllCoroutines();
+    }
+}
